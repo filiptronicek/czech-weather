ca25f09a89abb90c9603aee119012d5696f128f4
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-25 23:07:59 +0200

    Change field

--- daily.py
+++ daily.py
@@ -7,7 +7,7 @@ from os import environ, stat, path, access, R_OK, mkdir
 if environ.get('API_key') is None:
     from creds import API_key
 
-fields = ["time", "windspeed", "humidity", "temperature", "status"]
+fields = ["date", "windspeed", "humidity", "temperature", "status"]
 
 now = datetime.now()
 
